614593f358edaebde3d20cea781d6388a93ec0f3
Cheyanby Guiao <cheyanbynygel2801@gmail.com>
2025-02-12 12:09:32 +0800

    No code changes detected; skipping commit.

--- js/web.js
+++ js/web.js
@@ -1,62 +1,19 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Show the popup when the page loads
-    document.getElementById("imagePopup").style.display = "block";
-    fadeIn(document.getElementById("imagePopup"));
-  });
-  
-  function closePopup() {
-    // Fade out the popup and then hide it
-    fadeOut(document.getElementById("imagePopup"), function() {
-        document.getElementById("imagePopup").style.display = "none";
-    });
+  const links = document.querySelectorAll('a[href^="#"]');
+
+  for (const link of links) {
+      link.addEventListener("click", function(event) {
+          event.preventDefault();
+
+          const targetId = this.getAttribute("href").substring(1);
+          const targetElement = document.getElementById(targetId);
+
+          if (targetElement) {
+              window.scrollTo({
+                  top: targetElement.offsetTop,
+                  behavior: "smooth"
+              });
+          }
+      });
   }
-  
-  // Fade in function
-  function fadeIn(element, duration = 300) {
-    let opacity = 0;
-    element.style.opacity = opacity;
-  
-    const interval = 10;
-    const deltaOpacity = 1 / (duration / interval);
-  
-    const fadeInterval = setInterval(() => {
-        opacity += deltaOpacity;
-        element.style.opacity = opacity;
-  
-        if (opacity >= 1) {
-            clearInterval(fadeInterval);
-        }
-    }, interval);
-  }
-  
-  // Fade out function
-  function fadeOut(element, callback, duration = 300) {
-    let opacity = 1;
-    element.style.opacity = opacity;
-  
-    const interval = 10;
-    const deltaOpacity = 1 / (duration / interval);
-  
-    const fadeInterval = setInterval(() => {
-        opacity -= deltaOpacity;
-        element.style.opacity = opacity;
-  
-        if (opacity <= 0) {
-            clearInterval(fadeInterval);
-            callback();
-        }
-    }, interval);
-  }
-  
-  document.addEventListener("DOMContentLoaded", function() {
-    // Show the popup when the page loads
-    document.getElementById("mobileimagePopup").style.display = "block";
-    fadeIn(document.getElementById("mobileimagePopup"));
-  });
-  
-  function mobileclosePopup() {
-    // Fade out the popup and then hide it
-    fadeOut(document.getElementById("mobileimagePopup"), function() {
-        document.getElementById("mobileimagePopup").style.display = "none";
-    });
-  }
+});
